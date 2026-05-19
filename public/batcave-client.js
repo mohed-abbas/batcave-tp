5e14b92e0da6b14be34a3becf22939a153be3bdc
Mohed Abbas <mohed332@gmail.com>
2026-05-19 16:10:29 +0200

    fix(client): wrap shared helpers in IIFE to avoid global scope collision

--- public/batcave-client.js
+++ public/batcave-client.js
@@ -1,31 +1,33 @@
-const CREDS_KEY = 'batcave_creds';
+(function () {
+  const CREDS_KEY = 'batcave_creds';
 
-const creds = {
-  get: () => sessionStorage.getItem(CREDS_KEY),
-  set: (value) => sessionStorage.setItem(CREDS_KEY, value),
-  clear: () => sessionStorage.removeItem(CREDS_KEY)
-};
+  const creds = {
+    get: () => sessionStorage.getItem(CREDS_KEY),
+    set: (value) => sessionStorage.setItem(CREDS_KEY, value),
+    clear: () => sessionStorage.removeItem(CREDS_KEY)
+  };
 
-function authHeaders(extra = {}) {
-  const c = creds.get();
-  return c ? { Authorization: 'Basic ' + c, ...extra } : extra;
-}
+  function authHeaders(extra = {}) {
+    const c = creds.get();
+    return c ? { Authorization: 'Basic ' + c, ...extra } : extra;
+  }
 
-function escapeHTML(value) {
-  return String(value)
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
-}
+  function escapeHTML(value) {
+    return String(value)
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;');
+  }
 
-async function postJSON(url, body, extraHeaders = {}) {
-  return fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', ...extraHeaders },
-    body: JSON.stringify(body)
-  });
-}
+  async function postJSON(url, body, extraHeaders = {}) {
+    return fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', ...extraHeaders },
+      body: JSON.stringify(body)
+    });
+  }
 
-window.Batcave = { creds, authHeaders, escapeHTML, postJSON };
+  window.Batcave = { creds, authHeaders, escapeHTML, postJSON };
+})();
